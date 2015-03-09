c8c77bb438391e2cf8938cef795f3992beb610b5
tizot <masset.camille@gmail.com>
2015-03-09 16:57:34 +0100

    Initial Commit

---
+++ modele_simulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  modele_simulation
+//
+//  Created by Camille MASSET on 09/03/2015.
+//  Copyright (c) 2015 PSC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
