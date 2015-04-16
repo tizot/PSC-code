41634f4f4b10eb3bd6f6d512da9028ba63ed8957
tizot <masset.camille@gmail.com>
2015-04-16 11:37:14 +0200

    Affichage des paramètres passés en arguments. Bugfix : réinitialisation à minuit.

--- modele_simulation/main.cpp
+++ modele_simulation/main.cpp
@@ -38,12 +38,11 @@ vector<double> modele(int dureeSimulation, int deltaT, int tailleEchantillon) { 
     vector<double> puissanceReseau = initPuissanceReseau(dureeSimulation);
     
     for (int i(0); i < tailleEchantillon; i++) {
-        //cout << "Coucou ! [i]" << endl;
         Vehicule *vehicule = new Vehicule(deltaT);
-        //*vehicule = flotte[i];
+        cout << "Véhicule " << (i+1) << endl;
         int temps(0);
         while(temps < dureeSimulation) {
-            if (temps > 0 && ((temps * deltaT) % 86400 == 0)) {
+            if (temps > 0 && ((temps * deltaT) % 1440 == 0)) {
                 vehicule->reinitJour(); // on réinitialise certaines données à 00h00 chaque jour
             }
             vehicule->simulation(temps, deltaT, puissanceReseau);
@@ -63,16 +62,29 @@ int main(int argc, const char * argv[]) {
     
     try {
         int deltaT = boost::lexical_cast<int>(argv[1]);
+        cout << "deltaT = " << deltaT << " minutes" << endl;
         try {
             int duree = boost::lexical_cast<int>(argv[2]);
+            int nbJour = duree*deltaT/1440;
+            int nbHeure = (duree*deltaT - nbJour*1440) / 60;
+            int nbMinute = (duree*deltaT - nbJour*1440 - nbHeure*60);
+            cout << "Durée = ";
+            if (nbJour > 0)
+                cout << nbJour << "j";
+            cout << nbHeure << "h" << nbMinute << endl;
             try {
-                int taille = atoi(argv[3]);
+                int taille = boost::lexical_cast<int>(argv[3]);
+                cout << "Nb de VE = " << taille << endl;
+                
+                cout << endl;
                 
                 vector<double> puissanceReseau = modele(duree, deltaT, taille);
-                for (int i(0); i < 48*12; i++) {
-                    cout << puissanceReseau[i] << " ";
+                for (int i(0); i < duree; i++) {
+                    cout << i << " : " << puissanceReseau[i] << endl;
                 }
                 cout << endl;
+                
+                cout << "Terminé avec +/- de succès..." << endl;
                 return 0;
             } catch (const boost::bad_lexical_cast &) {
                 cerr << "La taille doit être un nombre entiers de VE" << endl;
